0e514def11079099f9ee4d6ebbd5ff8950e9af3f
astlachesis <astlachesis@gmail.com>
2025-12-08 11:16:37 +0900

    디자인 관련 div 삭제

--- src/pages/clusters/containers/Resources/GpuMigProfiles/Detail/Status/index.jsx
+++ src/pages/clusters/containers/Resources/GpuMigProfiles/Detail/Status/index.jsx
@@ -122,7 +122,7 @@ const Status = props => {
     <>
       <div>
         <Panel title={t('RESOURCES_MIG_PROFILE')}>
-          <div className={styles.tabs}>
+          {/* <div className={styles.tabs}> */}
             <Tabs
               type="button"
               activeName={storageClassTab}
@@ -134,9 +134,9 @@ const Status = props => {
                 return <TabPanel key={index} label={t(obj)} name={obj} />
               })}
             </Tabs>
-          </div>
+          {/* </div> */}
 
-          <div className={styles.wrapper}>
+          {/* <div className={styles.wrapper}> */}
             <div className="gpu_mig_container mig_profile_view">
               {
                 sortByGpuKey(detailData.gpuTypeDetail.filter(item => {                  
@@ -158,7 +158,7 @@ const Status = props => {
                   })
                 }
             </div>
-          </div>
+          {/* </div> */}
         </Panel>
       </div>
     </>
